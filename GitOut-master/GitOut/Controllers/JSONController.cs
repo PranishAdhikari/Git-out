768a657b66eb2e2cc6944fd22a4a2cb9ea07b684
pranish <adhikapi@mail.uc.edu>
2024-11-02 02:57:49 -0400

    Commit

---
+++ GitOut-master/GitOut/Controllers/JSONController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitOut.Controllers
+{
+    public class JSONController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetJson()
+        {
+            string output = false ? "Pranish Adhikari" : "Pranish Adhikari";
+
+            return Json(output);
+        }
+    }
+}
---
+++ GitOut-master/GitOut/Controllers/Name.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitOut.Controllers
+{
+    public class Name : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult name()
+        {
+            string output = false ? "Pranish Adhikari": "Pranish Adhikari";
+
+            return Json(output);
+        }
+    }
+}
